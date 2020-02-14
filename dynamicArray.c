e9d19616347efc50ad8638acbff616725e6ed66c
Ryan Murphy <46574034+github99er@users.noreply.github.com>
2020-02-14 01:06:27 -0500

    Update dynamicArray.c

--- dynamicArray.c
+++ dynamicArray.c
@@ -7,7 +7,7 @@ struct DynArr
 {
 	TYPE* data;		/* pointer to the data array */
 	int size;		/* Number of elements in the array */
-	int capacity;	/* capacity ofthe array */
+	int capacity;		/* capacity of the array */
 };
 
 
